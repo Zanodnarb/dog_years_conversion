eae90a987f965a706434195489ab325059ec6ebd
zanodnarb <103510789+Zanodnarb@users.noreply.github.com>
2022-07-15 08:17:08 -0400

    create app.js
    created file that converts human years into dog years and console logs it

---
+++ app.js
@@ -0,0 +1,9 @@
+const myAge = 26 // set var with my current age
+let earlyYears = 2 // the first 2 dogs years age at different rate than years after
+earlyYears *= 10.5
+let laterYears = myAge - 2
+laterYears *= 4
+myAgeInDogYears = laterYears + earlyYears
+console.log(myAgeInDogYears)
+myName = 'Brandon Adams'.toLowerCase()
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
